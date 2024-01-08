20dfb4b83980013ed9ca45bc0def352122cfab19
royalhoneybadger <takethejess@gmail.com>
2024-01-08 22:56:50 +0100

    "sidebar changed"

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import "./Sidebar.css"
+import {RssFeed} from "@mui/icons-material"
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
-      
+      <div className="sidebar-wrapper">
+        <ul className="sidebar-list">
+          <li className="sidebar-list-item">
+              <RssFeed className='sidebar-icon'/>
+              <span className="sidebar-list-item-text">Feed</span>
+          </li>
+          <li className="sidebar-list-item">
+              <RssFeed className='sidebar-icon'/>
+              <span className="sidebar-list-item-text">Feed</span>
+          </li>
+          <li className="sidebar-list-item">
+              <RssFeed className='sidebar-icon'/>
+              <span className="sidebar-list-item-text">Feed</span>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
